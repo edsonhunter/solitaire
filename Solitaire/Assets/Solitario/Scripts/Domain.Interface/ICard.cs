47a1302592f7d1855ad150c2254bb6d569d34ba5
Edson Ferrão <edson@fanatee.com>
2021-09-18 16:09:34 -0300

    Add given assets

--- Solitaire/Assets/Solitario/Scripts/Domain.Interface/ICard.cs
+++ Solitaire/Assets/Solitario/Scripts/Domain.Interface/ICard.cs
@@ -1,7 +1,35 @@
-namespace Solitario.Scripts.Domain.Interface
+namespace Solitario.Domain.Interface
 {
     public interface ICard
     {
+        public Suit Suit { get; }
+        public Rank Value { get; }
         
+    }
+
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    public enum Rank
+    {
+        Unknown,
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
     }
 }
---
+++ Solitaire/Assets/Solitario/Scripts/Domain/Card.cs
@@ -0,0 +1,9 @@
+using Solitario.Domain.Interface;
+
+namespace Solitario.Domain
+{
+    public class Card : ICard
+    {
+        
+    }
+}
